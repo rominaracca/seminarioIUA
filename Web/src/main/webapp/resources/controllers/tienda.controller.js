f517486011e80f773db9f1e103b5890e70f3b20e
Romina Racca <romina.racca@gmail.com>
2016-05-09 16:06:50 -0300

    filtros

--- Web/src/main/webapp/resources/controllers/tienda.controller.js
+++ Web/src/main/webapp/resources/controllers/tienda.controller.js
@@ -2,4 +2,9 @@ angular.module('tienda')
 	.controller('TiendaController', 
 			['$scope', function($scope){
 		$scope.titulo="Tienda";
+		$scope.productos = [ 
+		  {descripcion:"Leche", precio:34.32},
+		  {descripcion:"Arroz", precio:34.50},
+		  {descripcion:"Vino", precio:34.60}
+		  ];
 	}]);
