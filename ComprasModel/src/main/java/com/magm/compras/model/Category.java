7952728f77acea9c1e4af66a08aed3846e7adfad
Nelson Rios <nelsonmatias9@hotmail.com>
2016-05-21 12:10:42 -0300

    Agregada anotacion json ignore en el atributo products

--- ComprasModel/src/main/java/com/magm/compras/model/Category.java
+++ ComprasModel/src/main/java/com/magm/compras/model/Category.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Set;
 
 
@@ -29,6 +31,7 @@ public class Category implements Serializable{
 	@Column
 	private String description;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy="category")
 	private Set<Product> products;
 	
