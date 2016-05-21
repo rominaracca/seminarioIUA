ccb97371124fe87bfb19fa61c8a06eb042a2090a
Romina Racca <romina.racca@gmail.com>
2016-05-21 12:44:06 -0300

    .

--- Web/src/main/java/com/magm/web/services/compras/ProductsRSController.java
+++ Web/src/main/java/com/magm/web/services/compras/ProductsRSController.java
@@ -19,8 +19,6 @@ import com.magm.core.bussiness.service.exception.ServiceException;
 import com.magm.core.exception.NotFoundException;
 import com.magm.web.services.Constants;
 
-import net.minidev.json.annotate.JsonIgnore;
-
 //@CrossOrigin(origins={"*"}, methods={"POST"}) Es para todo el controlador
 @RestController
 @RequestMapping(value = Constants.URL_PRODUCTS)
