7b6223ef3f23c0cd09b1e17f7544e06abbd90396
Romina Racca <romina.racca@gmail.com>
2016-05-21 12:43:21 -0300

    agrego category

--- Web/src/main/java/com/magm/web/spring/config/Beans.java
+++ Web/src/main/java/com/magm/web/spring/config/Beans.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.magm.compras.business.service.ICategoryService;
 import com.magm.compras.business.service.IProductService;
+import com.magm.compras.business.service.impl.CategoryService;
 import com.magm.compras.business.service.impl.ProductService;
+import com.magm.compras.persistence.ICategoryDAO;
 import com.magm.compras.persistence.IProductDAO;
+import com.magm.compras.persistence.hibernate.CategoryDAO;
 import com.magm.compras.persistence.hibernate.ProductDAO;
 
 @Configuration
@@ -26,4 +30,19 @@ public class Beans {
 	public IProductService productService(final IProductDAO productDAO) {
 		return new ProductService(productDAO);
 	}
+	
+	// DAO
+	@Bean
+	@Autowired
+	public ICategoryDAO categoryDAO(final SessionFactory sessionFactory) {
+		return new CategoryDAO(sessionFactory);
+	}
+
+	// Service
+	@Bean
+	@Autowired
+	public ICategoryService categoryService(final ICategoryDAO categoryDAO) {
+		return new CategoryService(categoryDAO);
+	}
+	
 }
