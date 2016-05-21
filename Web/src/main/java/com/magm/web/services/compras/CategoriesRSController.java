01558f3a183c35e42a803e2df73f9cfb18e7ceb2
Nelson Rios <nelsonmatias9@hotmail.com>
2016-05-21 17:31:21 -0300

    controlador de ws categories implementado

--- Web/src/main/java/com/magm/web/services/compras/CategoriesRSController.java
+++ Web/src/main/java/com/magm/web/services/compras/CategoriesRSController.java
@@ -3,10 +3,20 @@ package com.magm.web.services.compras;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.magm.compras.business.service.ICategoryService;
+import com.magm.compras.model.Category;
+import com.magm.core.bussiness.service.SimpleResponse;
+import com.magm.core.bussiness.service.exception.ServiceException;
+import com.magm.core.exception.NotFoundException;
 import com.magm.web.services.Constants;
 
 @RestController
@@ -17,5 +27,76 @@ public class CategoriesRSController {
 	@Autowired
 	private ICategoryService categoryService;
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Object> list(@RequestParam(value="description", defaultValue="") String q) {
+		try {
+			return new ResponseEntity<Object>(categoryService.list(q), HttpStatus.OK);
+		} catch (ServiceException e) {
+			LOG.error(e.getMessage(), e);
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@RequestMapping(value = "/{idCategory}/products", method = RequestMethod.GET)
+	public ResponseEntity<Object> list(@PathVariable int idCategory) {
+		try {
+			return new ResponseEntity<Object>(categoryService.listProducts(idCategory), HttpStatus.OK);
+		} catch (ServiceException e) {
+			LOG.error(e.getMessage(), e);
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Object> save(@RequestBody Category Category) {
+		try {
+			System.out.println(Category);
+			return new ResponseEntity<Object>(categoryService.save(Category), HttpStatus.OK);
+		} catch (ServiceException e) {
+			LOG.error(e.getMessage(), e);
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public ResponseEntity<Object> update(@RequestBody Category Category) {
+		try {
+			return new ResponseEntity<Object>(categoryService.update(Category), HttpStatus.OK);
+		} catch (ServiceException e) {
+			LOG.error(e.getMessage(), e);
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Object> delete(@PathVariable int id) {
+		try {
+			Category c = new Category();
+			c.setId(id);
+			categoryService.delete(c);
+			return ResponseEntity.ok().body(null);
+		} catch (ServiceException e) {
+			LOG.error(e.getMessage(), e);
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Object> get(@PathVariable int id) {
+		try {
+			Category c = categoryService.load(id);
+			return new ResponseEntity<Object>(c, HttpStatus.OK);
+		} catch (ServiceException e) {
+			LOG.error(e.getMessage(), e);
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<Object>(new SimpleResponse(-1, e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
 	
 }
---
+++ Web/src/test/java/com/magm/web/services/compras/CategoriesRSControllerTest.java
@@ -0,0 +1,126 @@
+package com.magm.web.services.compras;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.magm.compras.model.Category;
+import com.magm.web.services.Constants;
+import com.magm.web.services.TestUtil;
+import com.magm.web.spring.config.RootConfig;
+import com.magm.web.spring.config.ServletInitializer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { RootConfig.class, ServletInitializer.class })
+@WebAppConfiguration
+public class CategoriesRSControllerTest {
+	@Autowired
+	private WebApplicationContext ctx;
+
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void performCicle() throws Exception {
+		Category c = new Category();
+		c.setDescription("testCategory");
+
+		// Add
+		MvcResult mvcResult = mockMvc
+				.perform(post(Constants.URL_CATEGORIES).contentType(MediaType.APPLICATION_JSON)
+						.content(TestUtil.convertObjectToJsonBytes(c)).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andDo(print())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.id", greaterThan(0)))
+				.andExpect(jsonPath("$.description", is(c.getDescription())))
+				.andReturn();
+		String content = mvcResult.getResponse().getContentAsString();
+		int id = TestUtil.getIntValueFromJson(content, "id");
+
+		// Update
+		c.setId(id);
+		c.setDescription("Producto Test updated");
+		mockMvc.perform(put(Constants.URL_CATEGORIES).contentType(MediaType.APPLICATION_JSON)
+				.content(TestUtil.convertObjectToJsonBytes(c)).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.description", is(c.getDescription())));
+
+		// Load
+		mockMvc.perform(get(Constants.URL_CATEGORIES + "/" + id).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.description", is(c.getDescription())));
+
+		// Delete
+		mockMvc.perform(delete(Constants.URL_CATEGORIES + "/" + id).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+
+		// Load Not Found
+		mockMvc.perform(get(Constants.URL_CATEGORIES + "/" + id).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound()).andExpect(content().contentType("application/json;charset=UTF-8"));
+	}
+	
+	@Test
+	public void listCategories() throws Exception {
+		mockMvc.perform(get(Constants.URL_CATEGORIES).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8"));
+	}
+
+	@Test
+	public void loadCategoryNotFound() throws Exception {
+		mockMvc.perform(get(Constants.URL_CATEGORIES + "/-999").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound()).andExpect(content().contentType("application/json;charset=UTF-8"));
+	}
+
+	@Test
+	public void deleteCategoryNotFound() throws Exception {
+		mockMvc.perform(delete(Constants.URL_CATEGORIES + "/-999").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound()).andExpect(content().contentType("application/json;charset=UTF-8"));
+	}
+
+	@Test
+	public void updateCategoryNotFound() throws Exception {
+		Category c = new Category();
+		c.setId(-999);
+		mockMvc.perform(put(Constants.URL_CATEGORIES + "/").contentType(MediaType.APPLICATION_JSON)
+				.content(TestUtil.convertObjectToJsonBytes(c)).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound()).andExpect(content().contentType("application/json;charset=UTF-8"));
+	}
+
+	@Configuration
+	@EnableWebMvc
+	public static class TestConfiguration {
+
+		@Bean
+		public CategoriesRSController categoriesRSController() {
+			return new CategoriesRSController();
+		}
+	}
+}
