00597aa929ca3dd2a7ecc2ac5c9bd6171e4526a0
Romina Racca <romina.racca@gmail.com>
2016-05-20 21:40:07 -0300

    interfaces business

--- ComprasBusiness/src/main/java/com/magm/compras/business/service/ICategoryService.java
+++ ComprasBusiness/src/main/java/com/magm/compras/business/service/ICategoryService.java
@@ -2,13 +2,13 @@ package com.magm.compras.business.service;
 
 import java.util.List;
 
+import com.magm.compras.model.Category;
 import com.magm.compras.model.Product;
 import com.magm.core.bussiness.service.IGenericService;
 import com.magm.core.bussiness.service.exception.ServiceException;
 
-public interface IProductService extends IGenericService<Product, Integer> {
-	
-	public List<Product> list(String part) throws ServiceException;
-	public List<String> listUniqueTags() throws ServiceException;
+public interface ICategoryService extends IGenericService<Category, Integer>{
+
+	public List<Product> list(int idCategory) throws ServiceException;
 	
 }
