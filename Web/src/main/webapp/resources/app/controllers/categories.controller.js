5329eee598a03f0a253b96f9265815ea6de7486e
Romina Racca <romina.racca@gmail.com>
2016-05-22 20:48:55 -0300

    getProducts

--- Web/src/main/webapp/resources/app/controllers/categories.controller.js
+++ Web/src/main/webapp/resources/app/controllers/categories.controller.js
@@ -12,7 +12,7 @@
     function CategoryCtrl(categoriesService, $log) {
         var vm = this;
 
-        vm.categories = null;
+        vm.categories = [];
         //vm.seleccionarTarea = seleccionarTarea;
 
         activate();
--- Web/src/main/webapp/resources/app/services/categories.service.js
+++ Web/src/main/webapp/resources/app/services/categories.service.js
@@ -8,11 +8,21 @@
     categoriesService.$inject = ['$http', 'URL_API_CATEGORY'];
     
     function categoriesService($http, URL_API_CATEGORY) { 
-    	return {
-    		
-    		list: function(){
-    			return $http.get(URL_API_CATEGORY);
-    		}
+    	
+    	var factory = {
+    			list: list,
+    			getProducts: getProducts
     	};
+    	
+    	return factory;
+    	
+    	function list(){
+    		return http.get(URL_API_CATEGORY);
+  		}
+    	
+    	function getProducts(id){
+    		return $http.get(URL_API_CATEGORY + '/' + id + '/products');
+    	}
+    	
     }
 })();
