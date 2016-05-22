453a429f28aa6cb80315aac1849dcf9bfec01998
Romina Racca <romina.racca@gmail.com>
2016-05-22 10:58:51 -0300

    redefinicion de servicio

--- Web/src/main/webapp/resources/app/services/products.service.js
+++ Web/src/main/webapp/resources/app/services/products.service.js
@@ -1,23 +1,22 @@
 angular.module('compras')
-.constant('URL_API_ROOT', '../api/v1/product')
+.constant('URL_API_ROOT', '../api/v1/products')
 .factory('productsService', [ '$http', 'URL_API_ROOT', function($http, URL_API_ROOT){
-	//similar a productRSController
 	
 	return {
 		
 		list: function(){
-			return $http.get(URL_API_ROOT + '/list');
+			return $http.get(URL_API_ROOT);
 		},
 		search: function(q){
-			return $http.get(URL_API_ROOT + '/list/filter?q='+q);
+			return $http.get(URL_API_ROOT + '?description='+q);
 		},
 		add:  function(product){
 			return $http.post(URL_API_ROOT + '/', product);
 		},
 		update: function(product){
-			return $http.put(URL_API_ROOT + '/', product);
+			return $http.put(URL_API_ROOT + '/' + id, product);
 		},
-		delete: function(id){
+		remove: function(id){
 			return $http.delete(URL_API_ROOT + '/' + id);
 		}
 	};
